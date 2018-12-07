67ec6ba8189747dfb13b77d3af0328ceba7fe8ab
vitebo <vitebo51@gmail.com>
2018-12-07 17:48:40 -0200

    initial commit

---
+++ db/migrate/20181121013201_create_billings.rb
@@ -0,0 +1,12 @@
+class CreateBillings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :billings do |t|
+      t.integer :student_id
+      t.integer :desired_due_day
+      t.integer :installments_count
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
